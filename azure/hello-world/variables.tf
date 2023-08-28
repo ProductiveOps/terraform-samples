53a237dc5af14d14c781e8f5e2089f77e6fa0d1b
Salil Ponde <salil.ponde@gmail.com>
2023-08-28 14:35:59 +0530

    First commit

---
+++ azure/hello-world/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  default = "eastus"
+  description = "Location of the resource group"
+}
+
+variable "resource_group_name_prefix" {
+  default = "rg"
+  description = "Prefix for the resource group"
+}
